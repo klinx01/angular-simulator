acbb68fda0755cf89139b7da07547b4f918f9894
klinx01 <vadimkorotnan@gmail.com>
2026-03-30 15:02:39 +0300

    fix error

--- src/user.service.ts
+++ src/user.service.ts
@@ -14,6 +14,7 @@ export class UserService {
   messageService: MessageService = inject(MessageService)
   loaderService: LoaderService = inject(LoaderService);
   userApiService: UserApiService = inject(UserApiService);
+  
   private userSubject: BehaviorSubject<IUser[]> = new BehaviorSubject<IUser[]>([]);
   users$: Observable<IUser[]> = this.userSubject.asObservable();
 
